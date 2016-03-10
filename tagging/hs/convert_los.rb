e1b7019808b806f84809d4de8c2e94acb952b6f8
Dante Soares <dante.m.soares@rice.edu>
2016-03-10 13:23:50 -0600

    Handle weird rows

--- tagging/hs/convert_los.rb
+++ tagging/hs/convert_los.rb
@@ -31,7 +31,11 @@ Axlsx::Package.new do |package|
     output_sheet.add_row OUTPUT_HEADERS, style: bold
 
     input_book.each_row_streaming(offset: 1, pad_cells: true) do |row|
-      lo = row[0].value
+      lo_cell = row[0]
+      next if lo_cell.nil?
+      lo = lo_cell.value
+      next if lo.nil? || lo.empty?
+
       output_sheet.add_row [lo, convert_lo(lo)]
     end
   end
