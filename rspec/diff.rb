8dba6ab9aa620de64bc86a9f5b059e0ea23376d7
Dante Soares <dante.m.soares@rice.edu>
2017-12-13 17:16:03 -0600

    Added rspec error diff script

---
+++ rspec/diff.rb
@@ -0,0 +1,111 @@
+# Usage:
+# err = <<-ERR
+# (paste full rspec error string here, starting on "expected")
+# ERR
+# require 'diff'
+# o1, o2, r1, r2 = diff(err)
+#
+# o1 and o2 will contain the objects being compared, extracted from the string
+# r1 will contain which elements are in o2 but not in o1
+# r2 (if present) will contain which elements are in o1 but not in o2
+# r2 is omitted if using expect().to include()
+
+require 'rails'
+require 'rspec'
+
+METHOD_PATTERN = / to (\w+) /
+SYMBOL_HASHROCKET_PATTERN = /:(\w+)\s*=>\s*/
+STRING_HASHROCKET_PATTERN = /:?("[^"]+")\s*=>\s*/
+HASH_PATTERN = /\(a hash including ([^()]+)\)/
+COLLECTION_INCLUDING_PATTERN = /\(a collection including ([^()]+)\)/
+COLLECTION_EXACTLY_PATTERN = /\(a collection containing exactly ([^()]+)\)/
+ASSOCIATION_PATTERN = /#<ActiveRecord::(?:Association)?Relation \[([^\[\]]+)\]>/
+OBJECT_PATTERN = /#<([^\s]+) ([^<>]+)>/
+SPLIT_PATTERN = / to \w+ /
+
+def incl(o1, o2)
+  oo1 = case o1
+  when RSpec::Matchers::BuiltIn::Include, RSpec::Matchers::BuiltIn::ContainExactly
+    expected = o1.expected
+    expected.is_a?(Array) && expected.one? && Hash === expected.first ? expected.first : expected
+  else
+    o1
+  end
+
+  oo2 = case o2
+  when RSpec::Matchers::BuiltIn::Include, RSpec::Matchers::BuiltIn::ContainExactly
+    expected = o2.expected
+    expected.is_a?(Array) && expected.one? && Hash === expected.first ? expected.first : expected
+  else
+    o2
+  end
+
+  case oo2
+  when Hash
+    {}.tap do |hh|
+      oo2.each do |key, value|
+        result = incl(oo1[key], value) rescue value
+
+        hh[key] = result unless result.blank?
+      end
+    end
+  when Array
+    match_any = o1.is_a?(RSpec::Matchers::BuiltIn::Include) ||
+                o1.is_a?(RSpec::Matchers::BuiltIn::ContainExactly) ||
+                o2.is_a?(RSpec::Matchers::BuiltIn::Include) ||
+                o2.is_a?(RSpec::Matchers::BuiltIn::ContainExactly)
+
+    oo2.each_with_index.map do |value, ii|
+      if match_any
+        oo1.map do |oo1v|
+          incl(oo1v, value) rescue value
+        end.select(&:blank?).first
+      else
+        incl(oo1[ii], value) rescue value
+      end
+    end.reject(&:blank?)
+  else
+    (o1.is_a?(RSpec::Matchers::BuiltIn::BaseMatcher) ? o1.matches?(o2) : o1 == o2) ? nil : o2
+  end
+end
+
+def diff(str)
+  str = str.split('Diff:').first
+
+  method = METHOD_PATTERN.match(str)[1]
+
+  str = str.sub(/\s*expected\s+/, '').gsub('[secure]', '"[secure]"')
+
+  while !SYMBOL_HASHROCKET_PATTERN.match(str).nil? || !STRING_HASHROCKET_PATTERN.match(str).nil? do
+    str.gsub!(SYMBOL_HASHROCKET_PATTERN, '\1: ')
+    str.gsub!(STRING_HASHROCKET_PATTERN, '\1: ')
+  end
+
+  while !HASH_PATTERN.match(str).nil? ||
+        !COLLECTION_INCLUDING_PATTERN.match(str).nil? ||
+        !COLLECTION_EXACTLY_PATTERN.match(str).nil? do
+    str.gsub!(HASH_PATTERN, '#<RSpec::Matchers::BuiltIn::Include \1>')
+    str.gsub!(COLLECTION_INCLUDING_PATTERN, '#<RSpec::Matchers::BuiltIn::Include \1>')
+    str.gsub!(COLLECTION_EXACTLY_PATTERN, '#<RSpec::Matchers::BuiltIn::ContainExactly \1>')
+  end
+
+  str.gsub!(' and ', ', ')
+
+  while !OBJECT_PATTERN.match(str).nil? do
+    str.gsub!(ASSOCIATION_PATTERN, '[\1]')
+    str.gsub!(OBJECT_PATTERN, '\1.new(\2)')
+  end
+
+  o1, o2 = str.split(SPLIT_PATTERN).map { |str| eval str }
+
+  r1 = incl(o1, o2)
+  puts "+ #{r1.inspect}"
+
+  r2 = nil
+  if method != 'include'
+    r2 = incl(o2, o1)
+    puts "- #{r2.inspect}"
+  end
+
+  [ o1, o2, r1, r2 ]
+end
