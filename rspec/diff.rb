bc1f651848e024bcefdd8b47701a1b11a5594c4f
Dante Soares <dante.m.soares@rice.edu>
2017-12-14 14:50:15 -0600

    Updated rspec diff tool to support KindOf matchers

--- rspec/diff.rb
+++ rspec/diff.rb
@@ -20,7 +20,9 @@ HASH_PATTERN = /\(a hash including ([^()]+)\)/
 COLLECTION_INCLUDING_PATTERN = /\(a collection including ([^()]+)\)/
 COLLECTION_EXACTLY_PATTERN = /\(a collection containing exactly ([^()]+)\)/
 ASSOCIATION_PATTERN = /#<ActiveRecord::(?:Association)?Relation \[([^\[\]]+)\]>/
-OBJECT_PATTERN = /#<([^\s]+) ([^<>]+)>/
+OBJECT_PATTERN = /#<([^\s:]+(?:::[^\s:]+)*)(?::0x[0-9a-f]+)? ([^<>]+)>/
+VARIABLE_STRING_PATTERN = /@\w+=("[^"]+")/
+VARIABLE_OTHER_PATTERN = /@\w+=([^\s]+)/
 SPLIT_PATTERN = / to \w+ /
 
 def incl(o1, o2)
@@ -40,16 +42,29 @@ def incl(o1, o2)
     o2
   end
 
-  case oo2
-  when Hash
+  match = if o1.is_a?(RSpec::Matchers::BuiltIn::BaseMatcher)
+    o1.matches?(o2)
+  elsif o2.is_a?(RSpec::Matchers::BuiltIn::BaseMatcher)
+    o2.matches?(o1)
+  elsif o1.is_a?(RSpec::Mocks::ArgumentMatchers::KindOf)
+    o1 === o2
+  elsif o2.is_a?(RSpec::Mocks::ArgumentMatchers::KindOf)
+    o2 === o1
+  else
+    o1 == o2
+  end
+
+  return if match
+
+  if oo1.is_a?(Hash) && oo2.is_a?(Hash)
     {}.tap do |hh|
       oo2.each do |key, value|
-        result = incl(oo1[key], value) rescue value
+        result = incl(oo1[key], value)
 
         hh[key] = result unless result.blank?
       end
     end
-  when Array
+  elsif oo1.is_a?(Array) && oo2.is_a?(Array)
     match_any = o1.is_a?(RSpec::Matchers::BuiltIn::Include) ||
                 o1.is_a?(RSpec::Matchers::BuiltIn::ContainExactly) ||
                 o2.is_a?(RSpec::Matchers::BuiltIn::Include) ||
@@ -58,14 +73,14 @@ def incl(o1, o2)
     oo2.each_with_index.map do |value, ii|
       if match_any
         oo1.map do |oo1v|
-          incl(oo1v, value) rescue value
+          incl(oo1v, value)
         end.select(&:blank?).first
       else
-        incl(oo1[ii], value) rescue value
+        incl(oo1[ii], value)
       end
     end.reject(&:blank?)
   else
-    (o1.is_a?(RSpec::Matchers::BuiltIn::BaseMatcher) ? o1.matches?(o2) : o1 == o2) ? nil : o2
+    o2
   end
 end
 
@@ -94,6 +109,8 @@ def diff(str)
   while !OBJECT_PATTERN.match(str).nil? do
     str.gsub!(ASSOCIATION_PATTERN, '[\1]')
     str.gsub!(OBJECT_PATTERN, '\1.new(\2)')
+    str.gsub!(VARIABLE_STRING_PATTERN, '\1')
+    str.gsub!(VARIABLE_OTHER_PATTERN, '\1')
   end
 
   o1, o2 = str.split(SPLIT_PATTERN).map { |str| eval str }
